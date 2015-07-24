5f38435ee257b1e7d53de803f1e268ba3c8b19b0
Mark Repka <mark@repkam09.com>
2015-07-24 19:24:00 -0400

    Add a desktop install script that contains things I generally install

---
+++ desktop-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo add-apt-repository ppa:hikariknight/unix-runescape-client
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install i3 suckless-tools curl youtube-dl ffmpeg vlc gparted vim feh openjdk-8-jre compton git htop runescape
+sudo apt-get remove dunst
+

4ceec41a8f92a8866021623fb1deab760917d555
Mark Repka <mark@repkam09.com>
2015-07-25 12:13:26 -0400

    Add consolekit

--- desktop-install.sh
+++ desktop-install.sh
@@ -2,6 +2,6 @@
 sudo add-apt-repository ppa:hikariknight/unix-runescape-client
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install i3 suckless-tools curl youtube-dl ffmpeg vlc gparted vim feh openjdk-8-jre compton git htop runescape
+sudo apt-get install i3 suckless-tools curl youtube-dl ffmpeg vlc gparted vim feh openjdk-8-jre compton git htop runescape consolekit
 sudo apt-get remove dunst
 

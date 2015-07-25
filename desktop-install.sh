b120dbab293a854c1849724b6bcd4ef8738c4014
Mark Repka <mark@repkam09.com>
2015-07-25 12:28:31 -0400

    Desktop script and i3 config modified

--- desktop-install.sh
+++ desktop-install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# runescape client
 sudo add-apt-repository ppa:hikariknight/unix-runescape-client
+
+# install all existing updates
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install i3 suckless-tools curl youtube-dl ffmpeg vlc gparted vim feh openjdk-8-jre compton git htop runescape consolekit
+
+# personal desktop specific files
+sudo apt-get install i3 suckless-tools curl youtube-dl ffmpeg vlc gparted vim feh openjdk-8-jre git htop runescape consolekit
+
+# remove dunst to use the normal notifyosd
 sudo apt-get remove dunst
 
+# required for Steam
+sudo apt-get install libgl1-mesa-dri:i386, libgl1-mesa-glx:i386, libc6:i386

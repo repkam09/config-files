435574b05cbb235d32bf0bd10e6d4c29124c1829
repkam09 <mark@repkam09.com>
2018-11-26 16:47:14 -0500

    update script

--- digitalocean/basics.sh
+++ digitalocean/basics.sh
@@ -2,23 +2,36 @@
 
 USERNAME=mark
 
-echo Setting up user $USERNAME
+# Set up user account if it does not exist
+if [ ! -d "/home/$USERNAME" ]
+then
+	echo Setting up user $USERNAME
+	adduser --disabled-password --quiet --gecos "" $USERNAME
+	adduser $USERNAME sudo
+else
+	echo Skipping user creationg because $USERNAME exists
+fi
 
-# adduser --disabled-password --gecos "" username
-adduser --disabled-password --quiet --gecos "" $USERNAME
-adduser $USERNAME sudo
 
-mkdir -p /home/$USERNAME/.ssh/
-cp ~/.ssh/authorized_keys /home/$USERNAME/.ssh/authorized_keys
-chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/
-chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/authorized_keys
+# Set up ssh keys if they do not exist
+if [ ! -d "/home/$USERNAME/.ssh" ]
+then
+	mkdir -p /home/$USERNAME/.ssh/
+	cp ~/.ssh/authorized_keys /home/$USERNAME/.ssh/authorized_keys
+	chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/
+	chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/authorized_keys
+else
+	echo Skipping ssh key copy as .ssh already exists
+fi
 
+echo Running apt updates
 apt update
 apt upgrade
+
+echo Installing helpful tools...
 apt install tmux screen git
 
 
-echo You should now be able to log on via SSH rsa key as $USERNAME
 
 
 

240a0b62f737153e95d0985942470305d9236f20
Mark Repka <mark@repkam09.com>
2018-12-11 16:35:06 +0000

    openvpn files

--- digitalocean/openvpn/ovpn.sh
+++ digitalocean/openvpn/ovpn.sh
@@ -2,21 +2,21 @@
 sudo apt update
 sudo apt install openvpn
 
-echo Configuring openvnpn server
-sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
-sudo gzip -d /etc/openvpn/server.conf.gz
-mv server.conf server.conf.orig
+#echo Configuring openvnpn server
+#sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
+#sudo gzip -d /etc/openvpn/server.conf.gz
+#mv server.conf server.conf.orig
 
-cat /etc/openvpn/server.conf.orig | sed 's/;tls-auth ta.key 0/tls-auth ta.key 0/' | \                         sed 's/tls-auth ta.key 0/tls-auth ta.key 0 \\\nkey-direction 0\\\n/' | \                              sed 's/cipher AES-256-CBC/cipher AES-256-CBC\\\nauth SHA256/' | \                                     sed 's/;user /user /' | \                                                                             sed 's/;group /group /'
+#cat /etc/openvpn/server.conf.orig | sed 's/;tls-auth ta.key 0/tls-auth ta.key 0/' | \                         sed 's/tls-auth ta.key 0/tls-auth ta.key 0 \\\nkey-direction 0\\\n/' | \                              sed 's/cipher AES-256-CBC/cipher AES-256-CBC\\\nauth SHA256/' | \                                     sed 's/;user /user /' | \                                                                             sed 's/;group /group /'
 
 
-exit
+#exit
 
 wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.4/EasyRSA-3.0.4.tgz
 cd ~
 tar xvf EasyRSA-3.0.4.tgz
 
-cd EasyRSA-3.0.4/
+cd ~/EasyRSA-3.0.4/
 ./easyrsa init-pki
 ./easyrsa gen-req server nopass
 
@@ -30,7 +30,7 @@ echo Wait here until processing is finished on CA server...
 read -p "Press enter to continue"
 
 sudo cp /tmp/{server.crt,ca.crt} /etc/openvpn/
-cd EasyRSA-3.0.4/
+cd ~/EasyRSA-3.0.4/
 ./easyrsa gen-dh
 
 openvpn --genkey --secret ta.key

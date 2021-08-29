48f7b5f99ca7a9e3b95182cf11bbb3ddbfd928f6
Mark Repka <mark@repkam09.com>
2021-08-29 13:53:34 -0400

    Multi-distro scripts

---
+++ base/arch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install all existing updates
+sudo pacman -Syu 
+
+#------------------------------------------------------------------------
+
+# personal software set
+sudo pacman -Sy i3-wm dmenu i3status feh vlc vim gparted ffmpeg youtube-dl curl arandr 
+
+#------------------------------------------------------------------------
--- desktop-install.sh
+++ desktop-install.sh
@@ -1,43 +1,16 @@
 #!/bin/bash
+BASE=`echo "$(lsb_release -is)"`
 
-# install all existing updates
-sudo apt-get update
-sudo apt-get upgrade
+if [ $BASE == "Ubuntu" ]
+then
+bash ./base/ubuntu.sh
+fi
 
-#------------------------------------------------------------------------
 
-# personal software set 
-sudo apt-get install i3 suckless-tools curl youtube-dl ffmpeg vlc gparted vim feh arandr build-essential flatpak lxappearance gnome-software-plugin-snap gnome-software-plugin-flatpak network-manager-openvpn network-manager-openvpn-gnome xbacklight htop obs-studio 
-
-#------------------------------------------------------------------------
-
-# Install nodejs LTS
-VERSION=node_16.x
-DISTRO="$(lsb_release -s -c)"
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
-
-sudo apt-get update
-sudo apt-get install nodejs
-
-#------------------------------------------------------------------------
-
-# remove dunst to use the normal notifyosd
-sudo apt-get remove dunst
-
-#------------------------------------------------------------------------
-
-sudo -s -- << EOF
-wget -O - https://content.runescape.com/downloads/ubuntu/runescape.gpg.key | apt-key add -
-mkdir -p /etc/apt/sources.list.d
-echo "deb [arch=amd64] https://content.runescape.com/downloads/ubuntu trusty non-free" > /etc/apt/sources.list.d/runescape.list
-apt-get update
-apt-get install -y runescape-launcher
-EOF
-
-#------------------------------------------------------------------------
-
+if [ $BASE == "Manjaro" ]
+then
+bash ./base/arch.sh
+fi
 
 # Configure Git globally
 git config --global user.name "Mark Repka"
@@ -50,6 +23,7 @@ git config --list
 #------------------------------------------------------------------------
 
 # Add helper programs into the local bin
+mkdir -p ~/.local/bin/
 chmod +x ./bin/*
 cp ./bin/* ~/.local/bin/
 

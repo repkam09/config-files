982ce303369e6c4568ddbe9cbc6a36340dc2cdc4
Mark Repka <mark@repkam09.com>
2026-03-23 11:04:34 -0400

    add net

--- .repka/services/private/deploy.sh
+++ .repka/services/private/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 docker pull caddy:builder
+docker pull caddy:latest
+
+docker run --rm -v ./Caddyfile:/etc/caddy/Caddyfile caddy:latest caddy fmt --overwrite /etc/caddy/Caddyfile
+
 docker compose build && docker compose down && docker compose up -d
